c64058454b7a06cdce68a8362e8f3ce880bd6189
jahirarana <145511943+jahirarana@users.noreply.github.com>
2025-12-16 22:08:28 -0800

    added sfx to scrolls,keys and door openings

--- Echoes of the Eighth/Assets/Scripts/DoorOpen.cs
+++ Echoes of the Eighth/Assets/Scripts/DoorOpen.cs
@@ -43,7 +43,6 @@ public class DoorOpen : MonoBehaviour {
             currentDay >= unlockDay &&
             currentHour >= openHour &&
             currentHour < closeHour;
-
         // true if puzzle solved
         bool shouldBeOpen = puzzleSolved || timeWindowOpen;
 
--- Echoes of the Eighth/Assets/Scripts/KeyCollector.cs
+++ Echoes of the Eighth/Assets/Scripts/KeyCollector.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class KeyCollector : MonoBehaviour {
     public DoorOpen EightDoor;                  //Eight door that will open
+    public AudioClip pickupSFX;
+    public AudioSource source;
 
     [HideInInspector]
     public bool collectedKey = false;
 
     void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Player")) { 
-            Debug.Log("got key");
-            collectedKey = true;                 
-            Destroy(gameObject);
+            source.PlayOneShot(pickupSFX);
+            collectedKey = true; 
+            foreach (var r in GetComponentsInChildren<Renderer>())
+                r.enabled = false;
         }
     }
-    
 }
